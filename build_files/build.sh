c7d8b18bccb3c02db509d1192bc6f93634e1199f
Haruki Luna <hluna@snowyrealms.net>
2026-04-07 21:39:38 +0100

    dnf install code, instead dnf5

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,8 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux zsh code
+dnf5 install -y tmux zsh
+dnf install -y code
 
 # Use a COPR Example:
 #
